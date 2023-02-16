f4ebfe9e60523517098605ca71de0196f8484202
Mario Hurtado <mariohu2001@gmail.com>
2023-02-17 00:08:32 +0100

    [Tests] Test para AcquireReusable
    Cambios realizados:
    [modificado] ReusablePoolTest.java
    
    Detalles:
    En este test se comprueba que el sistema es capaz de obtener los objetos "Reusable"
    que se encuentran dentro del Vector de "Reusable" de la instancia de "ReusablePool".
    También se comprueba que el método lanza la excepción "NotFreeInstanceException"
    cuando el Vector de objetos "Reusable" de "ReusablePool" se encuentra vacío.
    
    También se han sustituido algunas funciones de assertEqual por assertNotNull.

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -9,13 +9,16 @@ package ubu.gii.dass.test.c01;
 
 import static org.junit.Assert.*;
 
+
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+
+import ubu.gii.dass.c01.NotFreeInstanceException;
 // Importamos el objeto ReusablePool
 import ubu.gii.dass.c01.ReusablePool;
-
 
 /**
  * @author alumno
@@ -43,18 +46,18 @@ public class ReusablePoolTest {
 	@Test
 	public void testGetInstance() {
 
-		//Obtenemos una instancia
+		// Obtenemos una instancia
 		ReusablePool rp1 = ReusablePool.getInstance();
 		// Comprobamos que la instancia no es nula
-		assertNotEquals(null, rp1);
+		assertNotNull(rp1);
 
 		// Probamos a obtener otra instancia
 		ReusablePool rp2 = ReusablePool.getInstance();
 		// Comprobamos que la instancia no es nula
-		assertNotEquals(null, rp2);
+		assertNotNull(rp2);
+
 		// Comprobamos que la instancia es la misma (Singleton)
 		assertEquals(rp1, rp2);
-		
 	}
 
 	/**
@@ -62,11 +65,30 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testAcquireReusable() {
-		fail("Not yet implemented");
+		// El objeto ReusablePool comienza con 2 objetos de clase Reusable
+		ReusablePool rp = ReusablePool.getInstance();
+
+		// Obtenemos los dos elementos, no debería lanzar excepción
+		try {
+			assertNotNull(rp.acquireReusable());
+			assertNotNull(rp.acquireReusable());
+		} catch (Exception e) {
+			fail();
+		}
+
+		// Intentamos obtener otro elemento estando el pool vacío.
+		// Deberá lanzar la excepción NotFreeInstanceExcepcion.
+		try {
+			rp.acquireReusable();
+		} catch (Exception e) {
+			assertEquals(e.getClass(), NotFreeInstanceException.class);
+		}
+
 	}
 
 	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 * Test method for
+	 * {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
 	 */
 	@Test
 	public void testReleaseReusable() {
